228661e01e195a74deff08b38f8360d4c77e3f4d
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2025-12-15 20:12:10 +0200

    main
    - Ran `gofmt`

--- cmd/yggmail/main.go
+++ cmd/yggmail/main.go
@@ -58,7 +58,7 @@ func main() {
 	smtpaddr := flag.String("smtp", "localhost:1025", "SMTP listen address")
 	imapaddr := flag.String("imap", "localhost:1143", "IMAP listen address")
 	multicast := flag.Bool("multicast", false, "Connect to Yggdrasil peers on your LAN")
-        mcastregexp := flag.String("mcastregexp", ".*", "Regexp for multicast")
+	mcastregexp := flag.String("mcastregexp", ".*", "Regexp for multicast")
 	password := flag.Bool("password", false, "Set a new IMAP/SMTP password")
 	passwordhash := flag.String("passwordhash", "", "Set a new IMAP/SMTP password (hash)")
 	flag.Var(&peerAddrs, "peer", "Connect to a specific Yggdrasil static peer (this option can be given more than once)")
@@ -103,7 +103,7 @@ func main() {
 		copy(sk, skBytes)
 	}
 	pk := sk.Public().(ed25519.PublicKey)
-	mailAddr_user := hex.EncodeToString(pk);
+	mailAddr_user := hex.EncodeToString(pk)
 	mailAddr := fmt.Sprintf("%s@%s", mailAddr_user, utils.Domain)
 	log.Printf("Mail address: %s\n", mailAddr)
 
@@ -133,7 +133,7 @@ func main() {
 			log.Println("The supplied passwords do not match")
 			os.Exit(1)
 		}
-		
+
 		// trim away whitespace of UTF-8 bytes now as string
 		finalPassword := strings.TrimSpace(string(password1))
 
@@ -150,19 +150,19 @@ func main() {
 		log.Println("Password for IMAP and SMTP has been updated!")
 		os.Exit(0)
 	case passwordhash != nil && *passwordhash != "":
-		var hash string = strings.TrimSpace(*passwordhash);
+		var hash string = strings.TrimSpace(*passwordhash)
 		if len(hash) == 0 {
-			log.Println("Password hash cannot be blank");
-			os.Exit(1);
+			log.Println("Password hash cannot be blank")
+			os.Exit(1)
 		}
-		
-		log.Printf("Using password hash: '%v'\n", hash);
+
+		log.Printf("Using password hash: '%v'\n", hash)
 
 		if _, err := bcrypt.Cost(([]byte)(hash)); err != nil {
-			log.Printf("The provided hash is invalid %v\n", err);
-			os.Exit(1);
+			log.Printf("The provided hash is invalid %v\n", err)
+			os.Exit(1)
 		} else if err := storage.ConfigSetPassword(hash); err != nil {
-			log.Println("Failed to set password: ", err);
+			log.Println("Failed to set password: ", err)
 			os.Exit(1)
 		}
 
